c6dab49a5bbb9e279f787aff42356f46b5fc9fe1
michaelogunn <mogunnaike96@gmail.com>
2025-10-13 20:44:40 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";  
+import React, { useState, useEffect } from "react";
 import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid";
 import Header from '../components/headerMovieList';
@@ -6,10 +6,10 @@ import FilterCard from "../components/filterMoviesCard";
 
 const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
-    const [nameFilter, setNameFilter] = useState("");
+  const [nameFilter, setNameFilter] = useState("");
   const [genreFilter, setGenreFilter] = useState("0");
 
-    const genreId = Number(genreFilter);
+  const genreId = Number(genreFilter);
 
   let displayedMovies = movies
     .filter((m) => {
@@ -23,6 +23,13 @@ const HomePage = (props) => {
     if (type === "name") setNameFilter(value);
     else setGenreFilter(value);
   };
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
 
 
   useEffect(() => {
@@ -44,16 +51,16 @@ const HomePage = (props) => {
       <Grid size={12}>
         <Header title={"Home Page"} />
       </Grid>
-      <Grid container sx={{flex: "1 1 500px"}}>
-        <Grid key="find" size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
-              <FilterCard
-      onUserInput={handleChange}
-      titleFilter={nameFilter}
-      genreFilter={genreFilter}
-    />
+      <Grid container sx={{ flex: "1 1 500px" }}>
+        <Grid key="find" size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }} sx={{ padding: "20px" }}>
+          <FilterCard
+            onUserInput={handleChange}
+            titleFilter={nameFilter}
+            genreFilter={genreFilter}
+          />
 
         </Grid>
-            <MovieList movies={displayedMovies} />
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
 
       </Grid>
     </Grid>
